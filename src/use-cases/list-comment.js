81f825dfd40dbd58e08e4f836b645c7a11c1cae8
Wanjiku <wanjikukariuki03@gmail.com>
2020-01-23 18:37:33 +0300

    created a use case to read comments

---
+++ src/use-cases/list-comment.js
@@ -0,0 +1,33 @@
+// injects the commentsdb as a dependency
+export default function makeListComments ({ commentsDb }) {
+    // calls/gets the m using the id
+    return async function listComments ({ postId } = {}) {
+        if (!postId) {
+            throw new Error("You must have a post id.")
+        }
+        const comments = await commentsDb.findByPostId ({
+            postId,
+            omitReplies: false
+            //find the comment and its replies
+        })
+        const nestedComments = nest(comments)
+        return nestedComments
+
+        // if this gets slow learn how to introduce caching
+        function nest (comments){
+            if(comments.length === 0){
+                return comments
+            }
+            return comments.reduce((nested, comment) =>{
+                comment.replies = comments.filter(
+                    reply => reply.replyToId === comment.id 
+                )
+                nest(comment.replies)
+                if(comment.replyToId == null){
+                    nested.push(comment)
+                }
+                return nested
+            },[])
+        }
+    }
+}
