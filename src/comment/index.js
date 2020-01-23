24f81c420ad721fca21259105f4868d9281a6fce
Wanjiku <wanjikukariuki03@gmail.com>
2020-01-23 15:19:18 +0300

    transferred entity contents to index file of the entity

---
+++ src/comment/index.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto'
+import Id from '../Id'
+import ipRegex from 'ip-regex'
+import sanitizeHtml from 'sanitze-html'
+import buildMakeComment from './comment'
+import buildMakeSource from './source'
+
+const makeSource = buildMakeSource({isValidIp})
+const makeComment = buildMakeSource({Id, md5, sanitize, makeSource })
+
+export default makeComment
+
+function isValidIp (ip) {
+    return ipRegex ({exact: true}).test(ip)
+}
+function md5 (text) {
+    return crypto
+    .createdHash('md5')
+    .update(text, 'utf-8')
+    .digest('hex')
+}
+
+function sanitize (text){
+    return sanitizeHtml(text, {
+        allowedIframeHostnames: ['codesandbox.io', 'repl.it']
+    })
+}
