de0e623e2409eba9b639c19e03f7530af8bd77a9
Wanjiku <wanjikukariuki03@gmail.com>
2020-03-18 14:25:57 +0300

    started writing callback function

---
+++ src/express-callback/index.js
@@ -0,0 +1,12 @@
+module.exports (
+    (req, res)=>{
+        const httpRequest = {
+            body: req.body,
+            params: req.params
+        }
+        const httpResponse = {
+        //finish the rest
+        }
+    }
+    
+)
